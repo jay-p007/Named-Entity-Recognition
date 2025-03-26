44b7f4ebedee2b73967039c971cb9e6862413afb
jay-p007 <154582415+jay-p007@users.noreply.github.com>
2025-03-26 18:07:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def start_fastapi():
     except requests.exceptions.ConnectionError:
         print("⚡ Starting FastAPI...")
         subprocess.Popen(
-            ["uvicorn", "src.api.app:app", "--host", "0.0.0.0", "--port", str(FASTAPI_PORT)],
+            ["uvicorn", "src.api.app:app", "--host", "0.0.0.0", "--port", str(FASTAPI_PORT), "--reload"],
             stdout=subprocess.DEVNULL,
             stderr=subprocess.DEVNULL,
         )
